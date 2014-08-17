348356b04774c9bc04c7a7b519ac3119222313d8
loint <loinguyentrung@gmail.com>
2014-08-17 15:56:17 +0700

    Initialize first version

---
+++ analys_dom.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Copyright (C) 2014 Penlook  
+ *                    Vietnam
+ *                    http://www.penlook.com
+ *                    support@penlook.com
+ *
+ * Authors:
+ *  Loi Nguyen <loint@penlook.com>
+ *  Tin Nguyen <tinntt@penlook.com>
+ *
+ * CSS Analyser is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public
+ * License as published  by the Free Software Foundation, version
+ * 3 of the License.
+ *
+ * CSS Analyser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License  along with stoney cloud.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
