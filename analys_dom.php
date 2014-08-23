c4717125aae06cabff630b70bcf7f1bccaac5e88
tinntt <ntttin132@gmail.com>
2014-08-22 23:55:10 -0700

    Complete analyze dom to array class name, array id name, array tag name.

--- analys_dom.php
+++ analys_dom.php
@@ -24,3 +24,42 @@
  * If not, see <http://www.gnu.org/licenses/>.
  *
  */
+ 
+$input = __DIR__.'/input/text.html';
+$html = file_get_contents($input); 
+$html_start = strpos($html,'<body>');
+$html_end = strpos($html,'</body>');
+$html_start += 6;
+$html = trim(substr($html, $html_start, $html_end-$html_start));
+ 
+$class_pattern = '/class=\"[A-Za-z0-9\-\s\_]+\"/';
+$id_pattern = '/id=\"[A-Za-z0-9\-\s\_]+\"/';
+$tag_pattern = '/<[A-za-z0-9]+/';
+$property_pattern = '/(class|\"|=|\<|id)/';
+
+preg_match_all($class_pattern, $html, $class, PREG_OFFSET_CAPTURE);
+preg_match_all($id_pattern, $html, $id, PREG_OFFSET_CAPTURE);
+preg_match_all($tag_pattern, $html, $tag, PREG_OFFSET_CAPTURE);
+
+$class 	= $class[0];
+$id 	= $id[0];
+$tag 	= $tag[0];
+
+$class_blocks = array();
+foreach ($class as $class_block) {
+	$class_blocks[] = preg_replace($property_pattern, '', $class_block[0]);
+} 
+
+$id_blocks = array();
+foreach ($id as $id_block) {
+	$id_blocks[] = preg_replace($property_pattern, '', $id_block[0]);
+} 
+
+$tag_blocks = array();
+foreach ($tag as $tag_block) {
+	$tag_blocks[] = preg_replace($property_pattern, '', $tag_block[0]);
+} 
+
+var_dump($class_blocks);
+var_dump($tag_blocks);
+var_dump($id_blocks);
